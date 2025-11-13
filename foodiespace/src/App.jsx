02f7bddebbf79766129af73427ed3047ffa335b7
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-11-13 15:40:44 +0600

    Add loading state to App component

--- foodiespace/src/App.jsx
+++ foodiespace/src/App.jsx
@@ -1,14 +1,28 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { ToastContainer, Zoom } from 'react-toastify';
 
 function App() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="h-20"></div>
+
+      <div className="flex justify-center items-center min-h-screen">
+        {isLoading ? (
+          <div className="flex items-center justify-center min-h-[60vh]">
+            <span className="loading loading-spinner loading-xl text-primary"></span>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </div>
+
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
