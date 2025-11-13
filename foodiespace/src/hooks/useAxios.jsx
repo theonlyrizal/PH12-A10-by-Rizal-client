97e735f86392922046321af9d0a293e40d2b4445
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-11-13 21:29:09 +0600

    firebase

--- foodiespace/src/hooks/useAxios.jsx
+++ foodiespace/src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5050',
+  baseURL: import.meta.env.VITE_API_URL || 'https://foodiespaceonline.web.app',
 });
 
 const useAxios = () => axiosInstance;
